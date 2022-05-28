0a35948085b00f4ba2131d84e4d33b9235e2b85d
dcmatheus <mathws.costag@gmail.com>
2022-05-28 19:13:03 -0300

    chore: configure Tailwind Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Camera } from 'react-feather';
 
 function App() {
-  return <Camera className="text-amber-200 bg-blue-800 h-screen w-screen" />;
+  return (
+    <div className="w-screen h-screen bg-primary-200 bg-">
+      <div className="flex items-center justify-center h-screen">
+        <Camera className="w-4/5 h-4/5 text-primary-100 bg-" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          100: "#FFFFFF",
+          200: "#F7F8FC",
+          300: "#F1F1F1",
+          300: "#F1F3F5",
+          400: "#E0E0E0",
+          500:"#757575",
+          600: "#495057",
+          700: "#424242",
+        },
+        primary: {
+          100: "#4263EB",
+          200: "#244677",
+        },
+        delete: "#FC3333",
+        success: "#9DF28F",
+      },
+    },
   },
   plugins: [],
 }
