4cbb428a2efa4f660f37122ba43481299d431838
dcmatheus <mathws.costag@gmail.com>
2022-06-08 17:32:46 -0300

    feat(api): add an interceptor before every request to the api that adds the token in the header

--- src/api/connect.ts
+++ src/api/connect.ts
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
-export default axios.create({
+const api = axios.create({
   baseURL: 'https://contacts-api.prd.parceirodaconstrucao.com.br',
   headers: {
     'content-type': 'application/json',
   },
 });
+
+api.interceptors.request.use((config) => {
+  const url = config.url || '';
+  const token = localStorage.getItem('token');
+  const exceptions = ['/auth/login'];
+
+  if (exceptions.includes(url) || !token) {
+    return config;
+  }
+
+  const headers = { ...config.headers, Authorization: `Bearer ${token}` };
+  const newConfig = { ...config, headers };
+  return newConfig;
+}, (error) => Promise.reject(error));
+
+export default api;
